3748c2d1809c5d3b05a25e37ce98a141dd86e18a
Mac <mac15001900@gmail.com>
2016-06-11 22:03:02 +0100

    Javadoced Game class

--- src/serverLogic/Game.java
+++ src/serverLogic/Game.java
@@ -15,9 +15,18 @@ public class Game {
 	private Stats stats;
 	private ArrayList<Interactable> machines;
 
+	/** Update the position of a single player
+	 * 
+	 * @param playerId Id of the player
+	 * @param position new position */
 	public void updatePosition(String playerId, Pos position) {
 		stats.getPlayers().values().forEach(p -> p.updatePosition(position));
 	}
+	/** Execute an action as a player. Call this when a player sends a new action (not movement).
+	 * 
+	 * @param playerId
+	 * @param action
+	 * @return */
 	public Optional<ActionResponse> tryAction(String playerId, Action action) {
 		Player player = stats.getPlayers().get(playerId);
 		switch (action) {
@@ -55,6 +64,7 @@ public class Game {
 		}
 	}
 
+	/** Get a map of player id's to positions */
 	public Map<String, Pos> getAllPositions() {
 		Hashtable<String, Pos> res = new Hashtable<String, Pos>();
 		for (Player entry : stats.getPlayers().values()) {
@@ -62,10 +72,14 @@ public class Game {
 		}
 		return res;
 	}
+	/** Get a position of a specific player
+	 * 
+	 * @param id Id of that player */
 	public Optional<Pos> getPosition(String id) {
 		return stats.getPlayer(id).map(p -> p.getPosition());
 	}
 
+	/** Get a map of player id's to their stats */
 	public Map<String, StatContainer> getAllStats() {
 		Hashtable<String, StatContainer> res = new Hashtable<String, StatContainer>();
 		for (Player entry : stats.getPlayers().values()) {
@@ -73,12 +87,20 @@ public class Game {
 		}
 		return res;
 	}
+	/** Get stats for a player
+	 * 
+	 * @param id Player id
+	 * @return Stats for a player. Empty Optional if player doesn't exist. */
 	public Optional<StatContainer> getStats(String id) {
 		return stats.getPlayer(id).map(p -> p.getStats());
 	}
 
 
 
+	/** Get the machine closest to a player
+	 * 
+	 * @param player
+	 * @return Intractable object closest to the player, if they can interact with it. Empty Optional if nothing is in range. */
 	private Optional<Interactable> getClosest(Player player) {
 		Interactable res = null;
 		for (Interactable machine : machines) {
