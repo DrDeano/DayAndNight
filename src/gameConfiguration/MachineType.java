0008c566147fdce1cd5365b8020fba62963f75f6
Mac <mac15001900@gmail.com>
2016-06-12 12:41:56 +0100

    Added pooltables

--- src/gameConfiguration/MachineType.java
+++ src/gameConfiguration/MachineType.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import serverLogic.CoffeeMachine;
 import serverLogic.Computer;
 import serverLogic.Sofa;
+import serverLogic.Pooltable;
 
 public enum MachineType {
-	COFFEE(CoffeeMachine.class), SOFA(Sofa.class), COMPUTER(Computer.class);
+	COFFEE(CoffeeMachine.class), SOFA(Sofa.class), COMPUTER(Computer.class), POOL(Pooltable.class);
 
 
 	private final Class<?> type;
---
+++ src/serverLogic/Pooltable.java
@@ -0,0 +1,49 @@
+package serverLogic;
+
+import gameConfiguration.MachineType;
+
+public class Pooltable extends Interactable {
+
+	private double useTime;
+	private double sabotageTime;
+	private double funRestored;
+	private double funRestoredWhenSabotaged;
+
+
+
+	public Pooltable(int x, int y, int width, int height, double useTime, double sabotageTime, double funRestored,
+		double funRestoredWhenSabotaged) {
+		super(x, y, width, height, MachineType.POOL);
+		this.useTime = useTime;
+		this.sabotageTime = sabotageTime;
+		this.funRestored = funRestored;
+		this.funRestoredWhenSabotaged = funRestoredWhenSabotaged;
+	}
+
+	@Override
+	public double startUsing(Player player) {
+		return useTime;
+	}
+
+	@Override
+	public double startSabotaging(Player player) {
+		return sabotageTime;
+	}
+
+	@Override
+	public void cancelAction(Player player) {
+
+	}
+
+	@Override
+	public void finishUsing(Player player) {
+		player.changeStat(Stat.FUN, sabotaged ? funRestored : funRestoredWhenSabotaged);
+		sabotaged = false;
+	}
+
+	@Override
+	public void finishSabotaging(Player player) {
+		sabotaged = true;
+	}
+
+}
